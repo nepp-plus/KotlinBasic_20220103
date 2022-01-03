8b04f81c6ea17d19f9792eb771d2ff0d98f4a0c3
nepp-plus <nepp.plus@gmail.com>
2022-01-03 16:04:03 +0900

    토스트 띄워보기

--- app/src/main/java/com/neppplus/kotlinbasic_20220103/MainActivity.kt
+++ app/src/main/java/com/neppplus/kotlinbasic_20220103/MainActivity.kt
@@ -3,6 +3,7 @@ package com.neppplus.kotlinbasic_20220103
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -15,6 +16,15 @@ class MainActivity : AppCompatActivity() {
 
 //            버튼 눌리면 할일.
             Log.d("메인화면", "로그찍기 버튼 눌림")
+            Log.e("메인화면", "로그찍기 e로 로그")
+
+        }
+
+//        토스트 띄우기 버튼 눌리면 할 일.
+        btnToast.setOnClickListener {
+
+//            토스트로 "안녕하세요." 문장 띄우기
+            Toast.makeText(this, "안녕하세요.", Toast.LENGTH_SHORT).show()
 
         }
 
